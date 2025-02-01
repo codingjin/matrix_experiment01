d10f7c9bef85876e1f2f81ca3af17db4c9cb902a
codingjin <475166289@qq.com>
2025-02-01 02:56:56 -0700

    ansor openblas onemkl

---
+++ onemkl/batch.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+## 20 - thread
+./mm_onemkl 20 64 64 262144
+
+./mm_onemkl 20 128 128 65536
+
+./mm_onemkl 20 256 256 16384
+
+./mm_onemkl 20 512 512 4096
+
+./mm_onemkl 20 1024 1024 1024
+
+./mm_onemkl 20 2048 2048 256
+
+./mm_onemkl 20 4096 4096 64
+
+./mm_onemkl 20 8192 8192 16
+
+./mm_onemkl 20 16384 16384 4
+
+##
+
+./mm_onemkl 20 63 63 256000
+
+./mm_onemkl 20 125 125 64000
+
+./mm_onemkl 20 250 250 16000
+
+./mm_onemkl 20 500 500 4000
+
+./mm_onemkl 20 1000 1000 1000
+
+./mm_onemkl 20 2000 2000 250
+
+./mm_onemkl 20 4000 4000 63
+
+./mm_onemkl 20 8000 8000 15
+
+./mm_onemkl 20 16000 16000 3
+
+## 16 - thread
+./mm_onemkl 16 64 64 262144
+
+./mm_onemkl 16 128 128 65536
+
+./mm_onemkl 16 256 256 16384
+
+./mm_onemkl 16 512 512 4096
+
+./mm_onemkl 16 1024 1024 1024
+
+./mm_onemkl 16 2048 2048 256
+
+./mm_onemkl 16 4096 4096 64
+
+./mm_onemkl 16 8192 8192 16
+
+./mm_onemkl 16 16384 16384 4
+
+##
+
+./mm_onemkl 16 63 63 256000
+
+./mm_onemkl 16 125 125 64000
+
+./mm_onemkl 16 250 250 16000
+
+./mm_onemkl 16 500 500 4000
+
+./mm_onemkl 16 1000 1000 1000
+
+./mm_onemkl 16 2000 2000 250
+
+./mm_onemkl 16 4000 4000 63
+
+./mm_onemkl 16 8000 8000 15
+
+./mm_onemkl 16 16000 16000 3
+
+## 12 - thread
+./mm_onemkl 12 64 64 262144
+
+./mm_onemkl 12 128 128 65536
+
+./mm_onemkl 12 256 256 16384
+
+./mm_onemkl 12 512 512 4096
+
+./mm_onemkl 12 1024 1024 1024
+
+./mm_onemkl 12 2048 2048 256
+
+./mm_onemkl 12 4096 4096 64
+
+./mm_onemkl 12 8192 8192 16
+
+./mm_onemkl 12 16384 16384 4
+
+##
+
+./mm_onemkl 12 63 63 256000
+
+./mm_onemkl 12 125 125 64000
+
+./mm_onemkl 12 250 250 16000
+
+./mm_onemkl 12 500 500 4000
+
+./mm_onemkl 12 1000 1000 1000
+
+./mm_onemkl 12 2000 2000 250
+
+./mm_onemkl 12 4000 4000 63
+
+./mm_onemkl 12 8000 8000 15
+
+./mm_onemkl 12 16000 16000 3
+
+
+## 8 - thread
+./mm_onemkl 8 64 64 262144
+
+./mm_onemkl 8 128 128 65536
+
+./mm_onemkl 8 256 256 16384
+
+./mm_onemkl 8 512 512 4096
+
+./mm_onemkl 8 1024 1024 1024
+
+./mm_onemkl 8 2048 2048 256
+
+./mm_onemkl 8 4096 4096 64
+
+./mm_onemkl 8 8192 8192 16
+
+./mm_onemkl 8 16384 16384 4
+
+##
+
+./mm_onemkl 8 63 63 256000
+
+./mm_onemkl 8 125 125 64000
+
+./mm_onemkl 8 250 250 16000
+
+./mm_onemkl 8 500 500 4000
+
+./mm_onemkl 8 1000 1000 1000
+
+./mm_onemkl 8 2000 2000 250
+
+./mm_onemkl 8 4000 4000 63
+
+./mm_onemkl 8 8000 8000 15
+
+./mm_onemkl 8 16000 16000 3
+
+
+
---
+++ openblas/batch.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+## 20 - thread
+./mm_openblas 20 64 64 262144
+
+./mm_openblas 20 128 128 65536
+
+./mm_openblas 20 256 256 16384
+
+./mm_openblas 20 512 512 4096
+
+./mm_openblas 20 1024 1024 1024
+
+./mm_openblas 20 2048 2048 256
+
+./mm_openblas 20 4096 4096 64
+
+./mm_openblas 20 8192 8192 16
+
+./mm_openblas 20 16384 16384 4
+
+##
+
+./mm_openblas 20 63 63 256000
+
+./mm_openblas 20 125 125 64000
+
+./mm_openblas 20 250 250 16000
+
+./mm_openblas 20 500 500 4000
+
+./mm_openblas 20 1000 1000 1000
+
+./mm_openblas 20 2000 2000 250
+
+./mm_openblas 20 4000 4000 63
+
+./mm_openblas 20 8000 8000 15
+
+./mm_openblas 20 16000 16000 3
+
+## 16 - thread
+./mm_openblas 16 64 64 262144
+
+./mm_openblas 16 128 128 65536
+
+./mm_openblas 16 256 256 16384
+
+./mm_openblas 16 512 512 4096
+
+./mm_openblas 16 1024 1024 1024
+
+./mm_openblas 16 2048 2048 256
+
+./mm_openblas 16 4096 4096 64
+
+./mm_openblas 16 8192 8192 16
+
+./mm_openblas 16 16384 16384 4
+
+##
+
+./mm_openblas 16 63 63 256000
+
+./mm_openblas 16 125 125 64000
+
+./mm_openblas 16 250 250 16000
+
+./mm_openblas 16 500 500 4000
+
+./mm_openblas 16 1000 1000 1000
+
+./mm_openblas 16 2000 2000 250
+
+./mm_openblas 16 4000 4000 63
+
+./mm_openblas 16 8000 8000 15
+
+./mm_openblas 16 16000 16000 3
+
+## 12 - thread
+./mm_openblas 12 64 64 262144
+
+./mm_openblas 12 128 128 65536
+
+./mm_openblas 12 256 256 16384
+
+./mm_openblas 12 512 512 4096
+
+./mm_openblas 12 1024 1024 1024
+
+./mm_openblas 12 2048 2048 256
+
+./mm_openblas 12 4096 4096 64
+
+./mm_openblas 12 8192 8192 16
+
+./mm_openblas 12 16384 16384 4
+
+##
+
+./mm_openblas 12 63 63 256000
+
+./mm_openblas 12 125 125 64000
+
+./mm_openblas 12 250 250 16000
+
+./mm_openblas 12 500 500 4000
+
+./mm_openblas 12 1000 1000 1000
+
+./mm_openblas 12 2000 2000 250
+
+./mm_openblas 12 4000 4000 63
+
+./mm_openblas 12 8000 8000 15
+
+./mm_openblas 12 16000 16000 3
+
+
+## 8 - thread
+./mm_openblas 8 64 64 262144
+
+./mm_openblas 8 128 128 65536
+
+./mm_openblas 8 256 256 16384
+
+./mm_openblas 8 512 512 4096
+
+./mm_openblas 8 1024 1024 1024
+
+./mm_openblas 8 2048 2048 256
+
+./mm_openblas 8 4096 4096 64
+
+./mm_openblas 8 8192 8192 16
+
+./mm_openblas 8 16384 16384 4
+
+##
+
+./mm_openblas 8 63 63 256000
+
+./mm_openblas 8 125 125 64000
+
+./mm_openblas 8 250 250 16000
+
+./mm_openblas 8 500 500 4000
+
+./mm_openblas 8 1000 1000 1000
+
+./mm_openblas 8 2000 2000 250
+
+./mm_openblas 8 4000 4000 63
+
+./mm_openblas 8 8000 8000 15
+
+./mm_openblas 8 16000 16000 3
+
+
+
